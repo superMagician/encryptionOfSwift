db61e6a3dafcbcf8a40157adb4c05dcabbf7f6c5
YY <825241977@qq.com>
2017-08-31 17:47:24 +0800

    取消长度限制

--- encryptionOfSwift/Encryption.swift
+++ encryptionOfSwift/Encryption.swift
@@ -17,21 +17,18 @@ class Encryption: NSObject {
     //MARK: AES-ECB128加密
     public static func Endcode_AES_ECB(strToEncode:String)->String {
         // 从String 转成data
-        let ps = strToEncode.data(using: String.Encoding.utf8)
+        let data = strToEncode.data(using: String.Encoding.utf8)
         
         // byte 数组
         var encrypted: [UInt8] = []
         do {
-            encrypted = try AES(key: key, iv: iv, blockMode: .CBC, padding: PKCS7()).encrypt(ps!.bytes)
-        } catch AES.Error.dataPaddingRequired {
-            // block size exceeded
+            encrypted = try AES(key: key, iv: iv, blockMode: .CBC, padding: PKCS7()).encrypt(data!.bytes)
         } catch {
-            // some error
         }
         
-        let encoded = NSData.init(bytes: encrypted, length: encrypted.count)
+        let encoded =  Data(encrypted)
         //加密结果要用Base64转码
-        return encoded.base64EncodedString(options: NSData.Base64EncodingOptions.lineLength64Characters)
+        return encoded.base64EncodedString()
     }
     
     //  MARK:  AES-ECB128解密
@@ -41,27 +38,27 @@ class Encryption: NSObject {
         
         // byte 数组
         var encrypted: [UInt8] = []
+        let count = data?.length
+        
         // 把data 转成byte数组
-        for i in 0..<16 {
+        for i in 0..<count! {
             var temp:UInt8 = 0
             data?.getBytes(&temp, range: NSRange(location: i,length:1 ))
             encrypted.append(temp)
         }
+        
         // decode AES
         var decrypted: [UInt8] = []
         do {
             decrypted = try AES(key: key, iv: iv, blockMode:.CBC, padding: PKCS7()).decrypt(encrypted)
-        } catch AES.Error.dataPaddingRequired {
-            // block size exceeded
         } catch {
-            // some error
         }
         
         // byte 转换成NSData
-        let encoded = NSData.init(bytes: decrypted, length: decrypted.count)
+        let encoded = Data(decrypted)
         var str = ""
         //解密结果从data转成string
-        str = String(data: encoded as Data, encoding: String.Encoding.utf8)!
+        str = String(bytes: encoded.bytes, encoding: .utf8)!
         return str
     }
     
